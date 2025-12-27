ba1f369d0c6475be7b6bcd0bce7fe41db5a2a5fb
Kobos <kcoboscaballero@gmail.com>
2025-12-27 04:41:33 -0300

    desarrollo del modo oscuro y claro

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ function init() {
     renderPosts();
     renderCategoryFilter();
 }
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'Light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}
